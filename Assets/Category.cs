f0519cf19a42b05a9b995d1f5edcd086d8432e9a
Pierre POLLET <lopite2@live.fr>
2021-11-12 23:56:37 +0100

    Piti truc
    Premier petit truc pas trop utile

---
+++ Assets/Category.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Category
+{
+    public enum Couleur
+    {
+        BLUE,
+        ORANGE
+    }
+
+    public enum Type
+    {
+        ORGANIC,
+        CARDBOARD,
+        HOME_APPLIANCE
+    }
+}
